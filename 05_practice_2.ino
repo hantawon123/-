65c1c85f0551c4f60a2458ca76ea225c9c8d1edb
한태원(20203161) <66599007+hantawon123@users.noreply.github.com>
2020-09-15 07:09:14 +0900

    Create 05_practice_2.ino

---
+++ 05_practice_2.ino
@@ -0,0 +1,52 @@
+#define PIN_7
+unsigned int count, toggle,cnt;
+
+void setup() {
+  pinMode(PIN7, OUTPUT);
+  Serial.begin(115200); // Initialize serial port
+  while (!Serial) {
+    ; // wait for serial port to connect.
+  }
+  Serial.println("Hello World!");
+  count = toggle = 1;
+  cnt = 0;
+  digitalWrite(PIN7, toggle); // turn off LED.
+}
+
+void loop() {
+  Serial.println(cnt);
+  if(cnt<=10)
+    toggle = toggle_state(toggle);
+  else
+  {
+    while(1)
+    {
+      digitalWrite(PIN7, 1);
+    }
+  }
+  digitalWrite(PIN7, toggle); // update LED status.
+  if(cnt ==0)
+  {
+    cnt+=1;
+    delay(1000);
+  }
+    
+  else if(cnt<=10)
+  {
+    delay(100);
+    cnt+=1;
+  }
+}
+
+int toggle_state(int toggle) {
+  if(toggle == 0)
+  {
+    toggle = 1;
+    return toggle;
+  }
+  else
+  {
+    toggle = 0;
+    return toggle;
+  }
+}
